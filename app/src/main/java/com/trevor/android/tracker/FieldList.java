9eea0880476f96048cde53239f5ffb12d35ced53
trevor <vicentejverde@gmail.com>
2017-06-22 14:17:32 -0700

    add scrolling view

---
+++ app/src/main/java/com/trevor/android/tracker/FieldList.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.trevor.android.tracker;
+
+public final class FieldList {
+
+    /**
+     * This method returns a list of popular toy names from the 20th and early 21st centuries.
+     * I don't know about you guys, but this definitely brings me back to my childhood...
+     *
+     * @return A list of popular toys
+     */
+    public static String[] geFieldNames() {
+        return new String[] {
+                "Date",
+                "Period Start",
+                "Period End",
+                "Spotting",
+                "Cramping",
+                "Headache",
+                "Shoulder Pain",
+                "Sore Throat",
+                "Steps",
+                "Sleep Time",
+                "Insomnia",
+                "Nightmares",
+                "Call During Night",
+                "Work Hours",
+                "Diarrhea",
+                "Food Cravings",
+                "Unreasonable Hunger",
+                "Emotion",
+                "Focus",
+                "Energy",
+                "Sex PIV",
+                "Sex PIA",
+                "Sex Oral",
+                "High Sex Drive",
+        };
+    }
+}
--- app/src/main/java/com/trevor/android/tracker/MainActivity.java
+++ app/src/main/java/com/trevor/android/tracker/MainActivity.java
@@ -2,12 +2,25 @@ package com.trevor.android.tracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declare a TextView variable
+    TextView mFieldsListTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Use findViewById to get a reference to the TextView from the layout
+        mFieldsListTextView = (TextView) findViewById(R.id.field_names);
+        //Use the  FieldList.geFieldNames method and store the names in a String array called fieldNames
+        String[] fieldNames = FieldList.geFieldNames();
+
+        //Loop through each toy and append the name to the TextView (add \n for spacing)
+        for (String fieldName : fieldNames) {
+            mFieldsListTextView.append(fieldName + "\n\n\n");
+        }
     }
 }
