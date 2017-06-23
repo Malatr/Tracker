334d90efc5b663caa0534c13147adb7710916ba5
trevor <vicentejverde@gmail.com>
2017-06-22 18:27:03 -0700

    partially implement AsyncTask for db query; no runtime errors introduced by the un-accessed code

--- app/src/main/java/com/trevor/android/tracker/MainActivity.java
+++ app/src/main/java/com/trevor/android/tracker/MainActivity.java
@@ -1,8 +1,14 @@
 package com.trevor.android.tracker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,4 +29,54 @@ public class MainActivity extends AppCompatActivity {
             mFieldsListTextView.append(fieldName + "\n\n\n");
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Within onCreateOptionsMenu, use getMenuInflater().inflate to inflate the menu
+        getMenuInflater().inflate(R.menu.overflow, menu);
+        //Return true to display menu
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Get the ID of the item that was selected and store it in itemThatWasClicked
+        int itemThatWasClicked = item.getItemId();
+        //If the item's ID is R.id.overflow,
+        // show a Toast and return true to tell Android that I've handled this menu click
+        if (itemThatWasClicked == R.id.overflow){
+            Context context = MainActivity.this;
+            String textToShow = "Menu Selected";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+
+            return true;
+        }
+        //ElseIf we do NOT handle the menu click,
+        // return super.onOptionsItemSelected to let Android handle the menu click
+        return super.onOptionsItemSelected(item);
+    }
+
+    // Create a class called DbQueryTask that extends AsyncTask<Object, Object, Cursor>
+    public class DbQueryTask extends AsyncTask<Object, Object, Cursor>{
+
+        // Override the doInBackground method to perform the query.
+        // Return the results.
+        @Override
+        protected Cursor doInBackground(Object... params) {
+            Context context = MainActivity.this;
+            String textToShow = "doInBackground executed";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return null;
+        }
+
+        // Override onPostExecute to display the results in the TextView
+        @Override
+        protected void onPostExecute(Cursor result) {
+            Context context = MainActivity.this;
+            String textToShow = "onPostExecute executed";
+            Toast.makeText(context, textToShow, Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
 }
