f7ec719cb328b48fd5f435c24d3b91be3cf76cc0
trevor <vicentejverde@gmail.com>
2017-06-24 22:41:58 -0700

    add child activity

--- app/src/main/java/com/trevor/android/tracker/ChildActivity.java
+++ app/src/main/java/com/trevor/android/tracker/ChildActivity.java
@@ -1,6 +1,6 @@
 package com.trevor.android.tracker;
 
-import androidc.content.Intent;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
---
+++ app/src/main/java/com/trevor/android/tracker/SQLUtils.java
@@ -0,0 +1,8 @@
+package com.trevor.android.tracker;
+
+/**
+ * Created by Trevor on 6/23/2017.
+ */
+
+public class SQLUtils {
+}
