1066d7a1d25fd9dbe9c20b7e774162c7653c949c
Malatr <vicentejverde@gmail.com>
2017-06-24 18:02:43 -0700

    Update ChildActivity.java

--- app/src/main/java/com/trevor/android/tracker/ChildActivity.java
+++ app/src/main/java/com/trevor/android/tracker/ChildActivity.java
@@ -1,6 +1,6 @@
 package com.trevor.android.tracker;
 
-import android.content.Intent;
+import androidc.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
@@ -16,32 +16,12 @@ public class ChildActivity extends AppCompatActivity {
         /* Typical usage of findViewById... */
         mDisplayText = (TextView) findViewById(R.id.text_view_child_display);
 
-        /*
-         * Here is where all the magic happens. The getIntent method will give us the Intent that
-         * started this particular Activity.
-         */
         Intent intentThatStartedThisActivity = getIntent();
 
-          /*
-         * Although there is always an Intent that starts any particular Activity, we can't
-         * guarantee that the extra we are looking for was passed as well. Because of that, we need
-         * to check to see if the Intent has the extra that we specified when we created the
-         * Intent that we use to start this Activity. Note that this extra may not be present in
-         * the Intent if this Activity was started by any other method.
-         * */
         if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
-            /*
-             * Now that we've checked to make sure the extra we are looking for is contained within
-             * the Intent, we can extract the extra. To do that, we simply call the getStringExtra
-             * method on the Intent. There are various other get*Extra methods you can call for
-             * different types of data. Please feel free to explore those yourself.
-             */
+         
             String textEntered = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
 
-            /*
-             * Finally, we can set the text of our TextView (using setText) to the text that was
-             * passed to this Activity.
-             */
             mDisplayText.setText(textEntered);
         }
     }
